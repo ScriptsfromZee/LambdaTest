cdd8adb4457c74ba968d7c77e352c772ad4c063f
Zogam <derekchizogam@gmail.com>
2025-01-21 12:53:37 +0100

    User orders First Product

--- cypress/e2e/LamdaTest.cy.js
+++ cypress/e2e/LamdaTest.cy.js
@@ -20,7 +20,7 @@ describe("LamdaTest Suite", () => {
     ).click();
   });
 
-  // User uses the Search Box 
+  // User uses the Search Box and Orders a Product
   cy.get('input[name="search"]').type('ipad Nano')
   cy.get('button[type="submit"]').click()
   cy.contains('ipad Nano').scrollIntoView().should('be.visible').click()
@@ -31,6 +31,20 @@ describe("LamdaTest Suite", () => {
   // User Fills in Billing Details 
   cy.get('input[id="input-payment-firstname"]').type('David')
   cy.get('input[id="input-payment-lastname"]').type('Jones')
+  cy.get('label[for="input-payment-company"]').type('Groot Associates')
+  cy.get('input[name="address_1"]').type('50 Lagos Express Way')
+  cy.get('input[name="address_2"]').type('55 Lagos Express Way')
+  cy.get('label[for="input-payment-city"]').type('Ikeja')
+  cy.get('input[name="postcode"]').type('500102')
+  cy.get('label[for="input-payment-country"]').select('Nigeria')
+  cy.get('select[id="input-payment-zone"]').select('Lagos')
+  cy.get('text area[id="input-comment"]').type('I have always wanted to get an Ipod')
+  cy.get(
+    'a[href="https://ecommerce-playground.lambdatest.io/index.php?route=information/information/agree&information_id=5"]'
+  ) .click()
+  cy.get('button[id="button-save"]').click()
+  
+  
 
 
   
